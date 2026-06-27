b5447625bfdef813560562cf42dcfa4c44421c68
Alexandru Cohal <alexandru.cohal@yahoo.com>
2026-06-27 21:21:41 +0200

    Add function for UART initialization. Add call to this function in setup function. Add guard for enabling the UART

--- MPS_HwNode/MPS_HwNode.ino
+++ MPS_HwNode/MPS_HwNode.ino
@@ -1,5 +1,50 @@
+#define ENABLE_UART           true
+#define UART_BAUD_RATE        9600
+#define UART_DELAY_INIT_MS    500
+
+#if (ENABLE_UART == true)
+/** 
+ * Initializes the UART.
+ *
+ * @param None
+ *
+ * @return The result of UART initialization.
+ */
+bool Serial_Init(void)
+{
+  bool RetVal = true;
+
+  Serial.begin(UART_BAUD_RATE);
+
+  // Wait until the serial connection is open
+  while (!Serial); 
+  delay(UART_DELAY_INIT_MS);
+
+  return RetVal;
+}
+#endif
+
 void setup() 
 {
+  bool RetVal = true; 
+#if (ENABLE_UART == true)
+  RetVal = Serial_Init();
+#endif 
+
+#if (ENABLE_UART == true)
+  Serial.println("Start of setup");
+#endif
+
+#if (ENABLE_UART == true)
+  if (RetVal == true)
+  {
+    Serial.println("End of setup. Result: OK");
+  }
+  else
+  {
+    Serial.println("End of setup. Result: FAIL");
+  }
+#endif
 }
 
 void loop() 
