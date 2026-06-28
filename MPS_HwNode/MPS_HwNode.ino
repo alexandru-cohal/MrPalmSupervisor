4550676e049527916a106ce98577f5b37711622a
Alexandru Cohal <alexandru.cohal@yahoo.com>
2026-06-28 20:15:25 +0200

    Add macros, object initialization and call to start function for temperature and humidity sensor. Add function for temperature and humidity sensor values reading. Add call to this function and UART printing of the obtained values in loop function

--- MPS_HwNode/MPS_HwNode.ino
+++ MPS_HwNode/MPS_HwNode.ino
@@ -1,6 +1,18 @@
+#include <DHT.h>
+
 #define ENABLE_UART           true
 #define UART_BAUD_RATE        9600
 #define UART_DELAY_INIT_MS    500
+
+#define TEMP_HUMID_SENS_PIN   2
+#define TEMP_HUMID_SENS_TYPE  DHT11
+
+#define LOOP_DELAY_MS         5000
+
+/**
+ * Temperature and humidity sensor object
+ */
+DHT TempHumidSens(TEMP_HUMID_SENS_PIN, TEMP_HUMID_SENS_TYPE);
 
 #if (ENABLE_UART == true)
 /** 
@@ -24,9 +36,34 @@ bool Serial_Init(void)
 }
 #endif
 
+/** 
+ * Reads the temperature (°C) and humidity (%) sensor values and calculates the apparent temperature value (°C).
+ *
+ * @param None
+ *
+ * @return The result of temperature and humidity sensor reading.
+ */
+bool TempHumidSens_Read(float *Temp, float *Humid, float *ApparentTemp)
+{
+  bool RetVal = true;
+
+  *Humid = TempHumidSens.readHumidity();
+  *Temp = TempHumidSens.readTemperature(); 
+  *ApparentTemp = TempHumidSens.computeHeatIndex(*Temp, *Humid, false);
+
+  if (isnan(*Temp) || isnan(*Humid)) 
+  {
+    RetVal = false;
+  }
+
+  return RetVal;
+}
+
 void setup() 
 {
   bool RetVal = true; 
+
+  // Initialize UART
 #if (ENABLE_UART == true)
   RetVal = Serial_Init();
 #endif 
@@ -35,6 +72,10 @@ void setup()
   Serial.println("Start of setup");
 #endif
 
+  // Initialize temperature and humidity sensor
+  TempHumidSens.begin();
+
+  // End of initializations
 #if (ENABLE_UART == true)
   if (RetVal == true)
   {
@@ -49,4 +90,34 @@ void setup()
 
 void loop() 
 {
+  bool RetVal = true;
+
+  // Read temperature and humidity sensor values and calculates apparent temperature value
+  float Temp = 0;
+  float Humid = 0;
+  float ApparentTemp = 0;
+
+  RetVal = TempHumidSens_Read(&Temp, &Humid, &ApparentTemp);
+
+  if (RetVal == true)
+  {
+#if (ENABLE_UART == true)
+    Serial.print("Humidity: ");
+    Serial.print(Humid);
+    Serial.print("%  Temperature: ");
+    Serial.print(Temp);
+    Serial.print("°C  Apparent Temperature: ");
+    Serial.print(ApparentTemp);
+    Serial.print(F("°C "));
+    Serial.println();
+#endif
+  } 
+  else
+  {
+#if (ENABLE_UART == true)
+    Serial.println("Failed to read from temperature and humidity sensor!");
+#endif
+  }
+
+  delay(LOOP_DELAY_MS);
 }
